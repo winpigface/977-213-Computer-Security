ab70ed268163ff5622648b14f78628ab48b06c05
winfigface <winfigface2537@gmail.com>
2024-04-01 13:21:02 +0700

    lab4 find generetor

--- lab4/mywork/Solve.py
+++ lab4/mywork/Solve.py
@@ -1,13 +1,6 @@
-def gcb(a, b):
-    if b == 0:
-        return abs(a)
-    else:
-        return gcb(b, a % b)
-
-
-def set_H(g, prime):
-    for i in range(1, prime):
-        if (g**i) % prime == 1:
-            print(i)
-            return False
-    return True
+def generator(g, prime,range_prime):
+    w = set()
+    for pows in range(1,prime):
+        num = pow(g,pows,prime)
+        w.add(num)
+    return len(w) == prime - 1
--- lab4/mywork/main.py
+++ lab4/mywork/main.py
@@ -1,6 +1,6 @@
 from pwn import *
 import re
-import olve
+import Solve
 
 io = remote("172.26.201.17", 2134)
 recv = io.recvline().decode("utf-8")
@@ -13,15 +13,19 @@ match num_question:
         print(question_text)
         prime_number = re.findall(r"Prime:\ (.*)", question_text)[0]
         prime_number = int(prime_number)
-        print(prime_number)
-        p = [2, 3, 5, 7]
-        for i in p:
-            print(i)
-            if Solve.set_H(i, prime_number):
-                print("Y")
-                io.sendline(str(i).encode())
-                print(io.recvline())
+        # z = {1,2,3,...,prime - 1}
+        range_prime = set(range(1,prime_number))
+        g = int()
+        print("Finding generator...")
+        for i in range(prime_number):
+            if(Solve.generator(i,prime_number,range_prime)):
+                g = i
                 break
             else:
-                print("N")
                 continue
+        io.sendline(str(g).encode())
+        question_text2 = io.recvrepeat(timeout=0.5).decode("utf-8")
+        print(question_text2)
+        
+        
+        
